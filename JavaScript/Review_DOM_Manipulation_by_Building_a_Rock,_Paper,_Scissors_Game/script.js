e3e5366e2c375e38875b988b16b1f316d5098e6c
studyboy23 <ccps980115@gmail.com>
2025-04-15 00:19:22 +0800

    finish rock paper scissors game

---
+++ JavaScript/Review_DOM_Manipulation_by_Building_a_Rock,_Paper,_Scissors_Game/script.js
@@ -0,0 +1,60 @@
+function getRandomComputerResult() {
+  const options = ["Rock", "Paper", "Scissors"];
+  const index = Math.floor(Math.random() * options.length);
+  return options[index];
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore += 1;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (userOption === computerResult) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore += 1;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${
+      playerScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+  resetGameBtn.style.display = "none";
+  optionsContainer.style.display = "block";
+  winnerMsgElement.innerText = "";
+  roundResultsMsg.innerText = "";
+}
